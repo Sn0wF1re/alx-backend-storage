f71ebbddb6effc4907c5e79251c3952371fec9f7
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-04 09:37:52 +0000

    Make email field not null and unique

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- create table users with id, email, name and country fields
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email varchar(255),
+	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
 	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
