6961b391620b1b9741bff0fefaa27b8b4403a634
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-10 14:50:13 +0000

    Define call_history decorator where every time the original function will be called, we will add its input parameters to one list in redis, and store its output into another list

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,6 +28,27 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    define a call_history decorator
+    to store the history of inputs and outputs for a particular function
+    """
+    key = method.__qualname__
+    inputs = key + ':inputs'
+    outputs = key + ':outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         """
@@ -37,6 +58,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Args:
