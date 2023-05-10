fae2f8de8ab6d01dd465f6e4c79c5d31e0556862
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-10 19:22:42 +0000

    Edit file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,11 +17,13 @@ def get_page(url: str) -> str:
         url: url to be queried
     Returns: HTML content
     """
-    r.set(f"cached{url}", count)
+    cached = 'cached:' + url
+    r.set(cached, count)
     response = requests.get(url)
-    r.incr(f"count{url}")
-    val = r.get(f"cached{url}")
-    r.set(f"cached{url}", val, ex=10)
+    countKey = 'count:' + url
+    r.incr(countKey)
+    val = r.get(cached)
+    r.set(cached, val, ex=10)
     return response.text
 
 
